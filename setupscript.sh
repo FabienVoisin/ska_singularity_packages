265b65d747be6290a783e64cccbd8e42ca693616
FabienVoisin <fabien.voisin@adelaide.edu.au>
2025-09-23 13:32:42 +0000

    Yandasoft templae definition files added 	modified:   setupscript.sh 	renamed:    spack-wsclean-3.6-casacore-template -> spack-wsclean-3.6-casacore.template 	new file:   spack-yandasoft-casacore.template 	new file:   toolchain_openmpi_4.1.8/spack-yandasoft-casacore_3.7.1_openmpi_4.1.8.singularity

--- setupscript.sh
+++ setupscript.sh
@@ -13,7 +13,10 @@ sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack
 
 sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-casacore_3.7.1.template > toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
 
-sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-wsclean-3.6-casacore-template > toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-wsclean-3.6-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+
+sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-yandasoft-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+
 if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-${MPI}_${MPIVERSION}.sif ]] 
 then
 	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-${MPI}_${MPIVERSION}.sif toolchain_${MPI}_${MPIVERSION}/spack-${MPI}_${MPIVERSION}.singularity
@@ -29,3 +32,9 @@ if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1-${MPI
 then
 	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
 fi
+
+if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1-${MPI}_${MPIVERSION}.sif ]] 
+then
+	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+fi
+
