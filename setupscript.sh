6159070085d38c69b52c8e4e4dc856effa90ee73
Ubuntu <ubuntu@ip-10-0-3-70.ap-southeast-2.compute.internal>
2026-08-28 00:27:34 +0000

    Switch to casacore 3.8 installation 	modified:   makefile.generic 	modified:   mssplitpatchappCMakeList.patch 	modified:   mssplitpatchappmainCMakeList.patch 	modified:   pawsey/spack-casacore_mpich.def 	new file:   pawsey/spack-devmssplit-casacore_amd.def 	modified:   setupscript.sh 	modified:   spack-casacore_3.7.1.template 	modified:   spack-casacore_3.7.1.template.old 	new file:   spack-casacore_3.8.1.template 	modified:   spack-casacore_mpich.def 	new file:   spack-devmssplit-casacore_amd.def

--- setupscript.sh
+++ setupscript.sh
@@ -11,13 +11,13 @@ fi
 
 sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-openmpi.template > toolchain_${MPI}_${MPIVERSION}/spack-${MPI}_${MPIVERSION}.singularity
 
-sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-casacore_3.7.1.template > toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-casacore_3.8.1.template > toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 
-sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-wsclean-3.6-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-wsclean-3.6-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 
-sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-yandasoft-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-yandasoft-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 
-sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-mssplit-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+sed -e "s|\#\#MPI\#\#|${MPI}|g" -e "s|\#\#MPIVERSION\#\#|${MPIVERSION}|g"  spack-mssplit-casacore.template > toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 
 if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-${MPI}_${MPIVERSION}.sif ]] 
 then
@@ -25,22 +25,22 @@ then
 fi
 
 
-if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.7.1_${MPI}_${MPIVERSION}.sif ]] 
+if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.8.1_${MPI}_${MPIVERSION}.sif ]] 
 then
-	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.7.1_${MPI}_${MPIVERSION}.sif  toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.8.1_${MPI}_${MPIVERSION}.sif  toolchain_${MPI}_${MPIVERSION}/spack-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 fi
  
-if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1-${MPI}_${MPIVERSION}.sif ]] 
+if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.8.1-${MPI}_${MPIVERSION}.sif ]] 
 then
-	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.8.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-wsclean-3.6-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 fi
 
-if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1-${MPI}_${MPIVERSION}.sif ]] 
+if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.8.1-${MPI}_${MPIVERSION}.sif ]] 
 then
-	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.8.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-yandasoft-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 fi
 
-if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.7.1-${MPI}_${MPIVERSION}.sif ]] 
+if [[ ! -f toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.8.1-${MPI}_${MPIVERSION}.sif ]] 
 then
-	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.7.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.7.1_${MPI}_${MPIVERSION}.singularity
+	singularity build  toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.8.1-${MPI}_${MPIVERSION}.sif   toolchain_${MPI}_${MPIVERSION}/spack-mssplit-casacore_3.8.1_${MPI}_${MPIVERSION}.singularity
 fi
